b43eb20de59e6cbd76fc6a8352be98c1e2d90a23
Ashutosh Kumar <ashutosh.kumar.26.004@gmail.com>
2024-10-09 00:26:37 +0530

    Add files via upload

---
+++ Details.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string Name, college, city;
+    cout<< "Enter the Name: "<<endl;
+    cin>>Name;
+    cout<<"Enter the Name of the college: "<<endl;
+    cin>> college;
+    cout<<"Enter the Name of the city: "<<endl;
+    cin>>city;
+    cout<<Name<<endl;
+    cout<<college<<endl;
+    cout<<city<<endl;
+    return 0;
+}
